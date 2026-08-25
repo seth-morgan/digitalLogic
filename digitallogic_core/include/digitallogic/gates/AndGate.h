84ac528e9296ba34902325b486f1a923a9268699
Seth Morgan <sethiswanted@gmail.com>
2026-08-25 13:17:35 -0600

    docs(AndGate.h): add file header and inline comments

--- digitallogic_core/include/digitallogic/gates/AndGate.h
+++ digitallogic_core/include/digitallogic/gates/AndGate.h
@@ -1,3 +1,9 @@
+/**
+ * @file AndGate.h
+ * @brief Declares the two-input AND gate used by the digital logic simulator.
+ * @author Seth Morgan
+ * @date 2026-08-25
+ */
 #pragma once
 
 #include "digitallogic/model/Gate.h"
@@ -12,6 +18,7 @@ class AndGate final : public Gate {
 public:
     explicit AndGate(ComponentId id);
 
+    // Returns True only if every input is True; False or Unknown otherwise.
     [[nodiscard]] SignalValue evaluate(const QVector<SignalValue>& inputs) const override;
 };
 
