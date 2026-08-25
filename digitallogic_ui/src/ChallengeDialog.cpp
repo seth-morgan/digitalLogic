1218e5fa64dd5a7d4835117188154fddd8ac7490
Seth Morgan <sethiswanted@gmail.com>
2026-08-25 13:31:48 -0600

    docs(ChallengeDialog.cpp): add file header and inline comments

--- digitallogic_ui/src/ChallengeDialog.cpp
+++ digitallogic_ui/src/ChallengeDialog.cpp
@@ -1,3 +1,9 @@
+/**
+ * @file ChallengeDialog.cpp
+ * @brief Modal dialog for selecting a challenge level from the catalog.
+ * @author Seth Morgan
+ * @date 2026-08-25
+ */
 #include "digitallogic/ui/ChallengeDialog.h"
 
 #include "digitallogic/challenge/ChallengeCatalog.h"
@@ -27,6 +33,7 @@ ChallengeDialog::ChallengeDialog(QWidget* parent)
     auto* buttons = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel, this);
     layout->addWidget(buttons);
 
+    // Accept only when a row is selected; store that index for the caller.
     connect(buttons, &QDialogButtonBox::accepted, this, [this]() {
         if (m_levelList->currentRow() >= 0) {
             m_selectedLevelIndex = m_levelList->currentRow();
