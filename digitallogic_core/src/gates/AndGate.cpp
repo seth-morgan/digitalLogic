d0fe4ee29ba782ef01a2bb33e4da2840a7e7f627
Seth Morgan <sethiswanted@gmail.com>
2026-08-25 13:18:27 -0600

    docs(AndGate.cpp): add file header and inline comments

--- digitallogic_core/src/gates/AndGate.cpp
+++ digitallogic_core/src/gates/AndGate.cpp
@@ -1,27 +1,37 @@
+/**
+ * @file AndGate.cpp
+ * @brief Implements AND-gate evaluation for the digital logic simulator.
+ * @author Seth Morgan
+ * @date 2026-08-25
+ */
 #include "digitallogic/gates/AndGate.h"
 
 namespace digitallogic {
 
 AndGate::AndGate(const ComponentId id)
-    : Gate(id, GateKind::And, 2)
+    : Gate(id, GateKind::And, 2) // Two-input AND by default
 {
 }
 
 SignalValue AndGate::evaluate(const QVector<SignalValue>& inputs) const
 {
+    // Need at least two resolved inputs to produce a known AND result.
     if (inputs.size() < 2) {
         return SignalValue::Unknown;
     }
 
     for (const SignalValue input : inputs) {
+        // Any false input forces the AND output low (short-circuit).
         if (input == SignalValue::False) {
             return SignalValue::False;
         }
+        // Unknown inputs make the whole result indeterminate.
         if (input == SignalValue::Unknown) {
             return SignalValue::Unknown;
         }
     }
 
+    // All inputs were True.
     return SignalValue::True;
 }
 
