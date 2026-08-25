3598f719a56ab214055d54e854259fc76a5c3921
Seth Morgan <sethiswanted@gmail.com>
2026-08-25 13:19:17 -0600

    docs(NotGate.h): add file header and inline comments

--- digitallogic_core/include/digitallogic/gates/NotGate.h
+++ digitallogic_core/include/digitallogic/gates/NotGate.h
@@ -1,3 +1,9 @@
+/**
+ * @file NotGate.h
+ * @brief Declares the single-input NOT (inverter) gate.
+ * @author Seth Morgan
+ * @date 2026-08-25
+ */
 #pragma once
 
 #include "digitallogic/model/Gate.h"
@@ -12,6 +18,7 @@ class NotGate final : public Gate {
 public:
     explicit NotGate(ComponentId id);
 
+    // Inverts the first input; returns Unknown when the input list is empty.
     [[nodiscard]] SignalValue evaluate(const QVector<SignalValue>& inputs) const override;
 };
 
