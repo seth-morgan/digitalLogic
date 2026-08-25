d1298eb2ba412f991d9046e421181b408d79fec1
Seth Morgan <sethiswanted@gmail.com>
2026-08-25 13:20:43 -0600

    docs(PinId.cpp): add file header and inline comments

--- digitallogic_core/src/model/PinId.cpp
+++ digitallogic_core/src/model/PinId.cpp
@@ -1,1 +1,7 @@
+/**
+ * @file PinId.cpp
+ * @brief Translation unit for PinId; definitions live in the header.
+ * @author Seth Morgan
+ * @date 2026-08-25
+ */
 #include "digitallogic/model/PinId.h"
