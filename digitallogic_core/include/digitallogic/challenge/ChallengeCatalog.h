c16a2699de9086f16913d349a64bc2963e9104c9
Seth Morgan <sethiswanted@gmail.com>
2026-08-25 13:27:17 -0600

    docs(ChallengeCatalog.h): add file header and inline comments

--- digitallogic_core/include/digitallogic/challenge/ChallengeCatalog.h
+++ digitallogic_core/include/digitallogic/challenge/ChallengeCatalog.h
@@ -1,3 +1,9 @@
+/**
+ * @file ChallengeCatalog.h
+ * @brief Declares the built-in catalog of challenge levels.
+ * @author Seth Morgan
+ * @date 2026-08-25
+ */
 #pragma once
 
 #include "digitallogic/challenge/ChallengeLevel.h"
@@ -11,9 +17,9 @@ namespace digitallogic {
  */
 class ChallengeCatalog final {
 public:
-  [[nodiscard]] static const QVector<ChallengeLevel>& levels();
-  [[nodiscard]] static int levelCount() noexcept;
-  [[nodiscard]] static const ChallengeLevel* levelAt(int index);
+    [[nodiscard]] static const QVector<ChallengeLevel>& levels();
+    [[nodiscard]] static int levelCount() noexcept;
+    [[nodiscard]] static const ChallengeLevel* levelAt(int index);
 };
 
 } // namespace digitallogic
