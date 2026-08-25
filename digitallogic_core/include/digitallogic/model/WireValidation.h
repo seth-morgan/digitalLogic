2ebfd6bfab50fe043bd23a15e62f82bd6236072a
Seth Morgan <sethiswanted@gmail.com>
2026-08-25 13:21:59 -0600

    docs(WireValidation.h): add file header and inline comments

--- digitallogic_core/include/digitallogic/model/WireValidation.h
+++ digitallogic_core/include/digitallogic/model/WireValidation.h
@@ -1,3 +1,9 @@
+/**
+ * @file WireValidation.h
+ * @brief Declares wire-connection validation results and user-facing messages.
+ * @author Seth Morgan
+ * @date 2026-08-25
+ */
 #pragma once
 
 namespace digitallogic {
@@ -7,10 +13,10 @@ namespace digitallogic {
  */
 enum class WireValidationResult {
     Ok,
-    SameComponent,
-    InvalidDestination,
-    InvalidSource,
-    InputAlreadyConnected
+    SameComponent,           // From and to belong to the same component
+    InvalidDestination,      // Destination is not a valid input pin
+    InvalidSource,           // Source is not a valid output pin
+    InputAlreadyConnected    // Destination input already has a wire
 };
 
 /**
