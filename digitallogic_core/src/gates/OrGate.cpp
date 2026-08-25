66498bb5bda4c6f6af60730c2b68ed3173d61f98
Seth Morgan <sethiswanted@gmail.com>
2026-08-25 13:19:14 -0600

    docs(OrGate.cpp): add file header and inline comments

--- digitallogic_core/src/gates/OrGate.cpp
+++ digitallogic_core/src/gates/OrGate.cpp
@@ -1,9 +1,15 @@
+/**
+ * @file OrGate.cpp
+ * @brief Implements OR-gate evaluation for the digital logic simulator.
+ * @author Seth Morgan
+ * @date 2026-08-25
+ */
 #include "digitallogic/gates/OrGate.h"
 
 namespace digitallogic {
 
 OrGate::OrGate(const ComponentId id)
-    : Gate(id, GateKind::Or, 2)
+    : Gate(id, GateKind::Or, 2) // Two-input OR by default
 {
 }
 
@@ -14,14 +20,17 @@ SignalValue OrGate::evaluate(const QVector<SignalValue>& inputs) const
     }
 
     for (const SignalValue input : inputs) {
+        // Any true input forces the OR output high (short-circuit).
         if (input == SignalValue::True) {
             return SignalValue::True;
         }
+        // Unknown inputs make the whole result indeterminate.
         if (input == SignalValue::Unknown) {
             return SignalValue::Unknown;
         }
     }
 
+    // All inputs were False.
     return SignalValue::False;
 }
 
