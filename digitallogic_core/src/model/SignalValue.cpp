7cf18296b8a3a6f4e933191d0409885f918ee957
Seth Morgan <sethiswanted@gmail.com>
2026-08-25 13:20:28 -0600

    docs(SignalValue.cpp): add file header and inline comments

--- digitallogic_core/src/model/SignalValue.cpp
+++ digitallogic_core/src/model/SignalValue.cpp
@@ -1,9 +1,16 @@
+/**
+ * @file SignalValue.cpp
+ * @brief Implements string conversion helpers for SignalValue.
+ * @author Seth Morgan
+ * @date 2026-08-25
+ */
 #include "digitallogic/model/SignalValue.h"
 
 namespace digitallogic {
 
 const char* toString(const SignalValue value)
 {
+    // Map enum values to short labels shown in the UI.
     switch (value) {
     case SignalValue::False:
         return "0";
