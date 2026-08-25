97b55d52553c725ef9729ed122fef0eb4cbfaa33
Seth Morgan <sethiswanted@gmail.com>
2026-08-25 13:19:24 -0600

    docs(NandGate.h): add file header and inline comments

--- digitallogic_core/include/digitallogic/gates/NandGate.h
+++ digitallogic_core/include/digitallogic/gates/NandGate.h
@@ -1,3 +1,9 @@
+/**
+ * @file NandGate.h
+ * @brief Declares the two-input NAND gate (AND followed by NOT).
+ * @author Seth Morgan
+ * @date 2026-08-25
+ */
 #pragma once
 
 #include "digitallogic/model/Gate.h"
@@ -12,6 +18,7 @@ class NandGate final : public Gate {
 public:
     explicit NandGate(ComponentId id);
 
+    // Returns False only if every input is True; True when any input is False.
     [[nodiscard]] SignalValue evaluate(const QVector<SignalValue>& inputs) const override;
 };
 
