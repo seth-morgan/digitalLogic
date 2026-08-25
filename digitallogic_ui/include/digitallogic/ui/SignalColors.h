38a6dafda51f83469992d7337c8111ad9913a09d
Seth Morgan <sethiswanted@gmail.com>
2026-08-25 13:30:49 -0600

    docs(SignalColors.h): add file header and inline comments

--- digitallogic_ui/include/digitallogic/ui/SignalColors.h
+++ digitallogic_ui/include/digitallogic/ui/SignalColors.h
@@ -1,3 +1,9 @@
+/**
+ * @file SignalColors.h
+ * @brief Maps SignalValue to sandbox display colors based on simulation state.
+ * @author Seth Morgan
+ * @date 2026-08-25
+ */
 #pragma once
 
 #include "digitallogic/model/SignalValue.h"
@@ -12,6 +18,7 @@ namespace digitallogic::ui {
  */
 [[nodiscard]] inline QColor signalColor(const SignalValue value, const bool simulated)
 {
+    // Idle/unknown wires stay muted until a simulation run paints true/false.
     if (!simulated || value == SignalValue::Unknown) {
         return AppTheme::wireIdle();
     }
