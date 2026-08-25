03d79db95b2e6ef49ade622f9009dbfdd8b7229d
Seth Morgan <sethiswanted@gmail.com>
2026-08-25 13:19:31 -0600

    docs(XorGate.cpp): add file header and inline comments

--- digitallogic_core/src/gates/XorGate.cpp
+++ digitallogic_core/src/gates/XorGate.cpp
@@ -1,9 +1,15 @@
+/**
+ * @file XorGate.cpp
+ * @brief Implements exclusive-OR (XOR) gate evaluation.
+ * @author Seth Morgan
+ * @date 2026-08-25
+ */
 #include "digitallogic/gates/XorGate.h"
 
 namespace digitallogic {
 
 XorGate::XorGate(const ComponentId id)
-    : Gate(id, GateKind::Xor, 2)
+    : Gate(id, GateKind::Xor, 2) // Exactly two inputs for XOR
 {
 }
 
@@ -13,10 +19,12 @@ SignalValue XorGate::evaluate(const QVector<SignalValue>& inputs) const
         return SignalValue::Unknown;
     }
 
+    // Either unknown input makes the XOR result indeterminate.
     if (inputs.at(0) == SignalValue::Unknown || inputs.at(1) == SignalValue::Unknown) {
         return SignalValue::Unknown;
     }
 
+    // True when the two bits differ (exclusive or).
     return inputs.at(0) != inputs.at(1) ? SignalValue::True : SignalValue::False;
 }
 
